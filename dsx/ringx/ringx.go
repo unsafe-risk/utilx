0df14418b778b4b8ca0b1f5c2800624ab0fd91d1
김창학 <sjc02183@naver.com>
2022-12-22 23:50:21 +0900

    Update ringx.go
    generic 처리

--- dsx/ringx/ringx.go
+++ dsx/ringx/ringx.go
@@ -2,44 +2,26 @@ package ringx
 
 import "fmt"
 
-func NewRing(cap int64) *Ring {
+type Ring[T any] struct {
+	data []T
+	cap  int64
+	head int64
+	tail int64
+}
+
+func New[T any](cap int64) *Ring[T] {
 	if cap == 0 {
 		return nil
 	}
-	return &Ring{
-		data: make([]interface{}, cap),
+	return &Ring[T]{
+		data: make([]T, cap),
 		cap:  cap,
 		head: 0,
 		tail: 0,
 	}
 }
 
-type Ring struct {
-	data []interface{}
-	cap  int64
-	head int64
-	tail int64
-}
-
-func (t *Ring) Cap() int64 {
-	return t.cap
-}
-
-func (t *Ring) IsEmpty() bool {
-	if t.head == t.tail {
-		return true
-	}
-	return false
-}
-
-func (t *Ring) IsFull() bool {
-	if t.head == (t.tail+1)%t.cap {
-		return true
-	}
-	return false
-}
-
-func (t *Ring) Enqueue(data interface{}) error {
+func (t *Ring[T]) Enqueue(data T) error {
 	if t.IsFull() == true {
 		return fmt.Errorf("Queue is full | cap - %d", t.cap)
 	}
@@ -48,15 +30,27 @@ func (t *Ring) Enqueue(data interface{}) error {
 	return nil
 }
 
-func (t *Ring) Dequeue() (data interface{}) {
+func (t *Ring[T]) Dequeue() (data T, ok bool) {
 	if t.IsEmpty() {
-		return nil
+		return
 	}
 	data = t.data[t.head]
 	t.head = (t.head + 1) % t.cap
-	return data
+	return data, true
 }
 
-func (t *Ring) Head() interface{} {
+func (t *Ring[T]) Head() T {
 	return t.data[t.head]
+}
+
+func (t *Ring[T]) Cap() int64 {
+	return t.cap
+}
+
+func (t *Ring[T]) IsEmpty() bool {
+	return t.head == t.tail
+}
+
+func (t *Ring[T]) IsFull() bool {
+	return t.head == (t.tail+1)%t.cap
 }
