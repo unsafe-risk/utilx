e232a8bcfcc444bdc779d77d5033fbf24723c5e1
gokch <sjc02183@naver.com>
2022-12-25 22:13:12 +0900

    make search, sort (#12)

--- algox/searchx/searchx.go
+++ algox/searchx/searchx.go
@@ -1,1 +1,22 @@
 package searchx
+
+import (
+	"golang.org/x/exp/constraints"
+	"golang.org/x/exp/slices"
+)
+
+func Search[T constraints.Ordered](s []T, x T) (int, bool) {
+	return SearchCustom(s, x, func(i, j T) int {
+		if i < j {
+			return -1
+		}
+		if i > j {
+			return 1
+		}
+		return 0
+	})
+}
+
+func SearchCustom[T any](s []T, x T, less func(i, j T) int) (int, bool) {
+	return slices.BinarySearchFunc(s, x, less)
+}
