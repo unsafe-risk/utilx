161bdd76a0bbaeff7661d02e24d9e6c2e15368d9
snowmerak <snowmerak@outlook.com>
2022-12-31 20:51:43 +0900

    make Logger object

--- logx/logx.go
+++ logx/logx.go
@@ -1,1 +1,38 @@
 package logx
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type logFrame struct {
+	Time  string `json:"time"`
+	Level Level  `json:"level"`
+}
+
+type Logger struct {
+	writers      []io.Writer
+	defaultLevel Level
+	timeformat   string
+}
+
+func NewLogger(writers ...io.Writer) (*Logger, error) {
+	if len(writers) < 1 {
+		return nil, fmt.Errorf("logx.NewLogger: %w", errNoWriter)
+	}
+	lg := &Logger{
+		writers:      writers,
+		defaultLevel: Info,
+		timeformat:   time.RFC3339,
+	}
+	return lg, nil
+}
+
+func (l *Logger) SetDefaultLevel(level Level) {
+	l.defaultLevel = level
+}
+
+func (l *Logger) SetTimeFormat(format string) {
+	l.timeformat = format
+}
